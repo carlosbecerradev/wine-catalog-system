b9a6db1e435e453237f961dfa366091c3082f051
Carlos Becerra01 <geminiscarlos.12@gmail.com>
2019-12-22 17:14:41 -0500

    api + dal funcionando

---
+++ TVWAPI/Controllers/CepaController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using TVWAPI.Models;
+using TVDAL;
+
+namespace TVWAPI.Controllers
+{
+    public class CepaController : ApiController
+    {
+
+        private DBModel objDB = new DBModel();
+
+        [HttpGet]
+        public IEnumerable<CepaViewModel> Get()
+        {
+            IList<CepaViewModel> lstCepa = null;
+
+            using (DBModel cn = new DBModel())
+            {
+                lstCepa = cn.Cepas.Select(s => new CepaViewModel()
+                {
+                    Id = s.Id,
+                    Nombre = s.Nombre
+                }).ToList<CepaViewModel>();
+
+                return lstCepa;
+            }
+        }
+
+        [HttpGet]
+        public CepaViewModel Get(int Id)
+        {
+            CepaViewModel cepa = new CepaViewModel();
+            using (DBModel cn = new DBModel())
+            {
+                var x = cn.Cepas.FirstOrDefault(e => e.Id == Id);
+                if (x != null)
+                {
+                    cepa.Id = x.Id;
+                    cepa.Nombre = x.Nombre;
+                }
+                return cepa;
+            }
+
+        }
+
+        [HttpPost]
+        public IHttpActionResult CrearCepa([FromBody]CepaViewModel cepa)
+        {
+            if (ModelState.IsValid)
+            {
+                objDB.Cepas.Add(new Cepa() { Nombre = cepa.Nombre });
+                objDB.SaveChanges();
+                return Ok(cepa);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult EditarCepa(int Id, [FromBody]CepaViewModel cepa)
+        {
+            if (ModelState.IsValid)
+            {
+                var regexiste = objDB.Cepas.Count(c => c.Id == Id) > 0;
+
+                if (regexiste)
+                {
+                    var x = objDB.Cepas.Find(Id);
+                    x.Nombre = cepa.Nombre;
+
+                    objDB.SaveChanges();
+                    return Ok(cepa);
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+        }
+
+        [HttpDelete]
+        public IHttpActionResult EliminarCepa(int Id)
+        {
+
+            var x = objDB.Cepas.Find(Id);
+
+            if (x != null)
+            {
+                objDB.Cepas.Remove(x);
+                objDB.SaveChanges();
+                return Ok(x);
+
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        }
+
+    }
+}
