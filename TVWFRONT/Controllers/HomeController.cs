0db236c8460ce4fcdc9bac9dfd64ae6b692e2b46
Carlos Becerra01 <geminiscarlos.12@gmail.com>
2019-12-23 18:26:56 -0500

    Adding login to front a cms, add input.class in normalizr

--- TVWFRONT/Controllers/HomeController.cs
+++ TVWFRONT/Controllers/HomeController.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using TVWFRONT.Models;
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace TVWFRONT.Controllers
 {
     public class HomeController : Controller
@@ -22,10 +29,60 @@ namespace TVWFRONT.Controllers
             return View();
         }
 
-        // GET: /Home/
+        private string BaseURL = "http://localhost:3212/api/Usuario";
+
+        // GET: Acceso
         public ActionResult Acceder()
         {
-            return View();
+            UsuarioViewModel oUsr = new UsuarioViewModel();
+            ViewBag.ErrorMsg = "";
+            return View(oUsr);
         }
+
+        [HttpPost]
+        public ActionResult Acceder(UsuarioViewModel usr)
+        {
+            UsuarioViewModel destUsr = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseURL);
+                //HTTP GET
+                var responseTask = client.GetAsync("Usuario?nombre=" + usr.Nombre + "&contrasenia=" + usr.Contrasenia);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<UsuarioViewModel>();
+                    readTask.Wait();
+
+                    destUsr = readTask.Result;
+
+                    if (destUsr.Id == -1) //no entró
+                    {
+                        ViewBag.ErrorMsg = "¡Usuario o clave incorrecta...!";
+                        Session["ssUsuario"] = null;
+                        return View(usr);
+                    }
+                    else
+                    {
+                        Session["ssUsuario"] = destUsr;
+
+                        if (destUsr.IdRole == 1) // ADMIN
+                        {
+                            return Redirect("http://localhost:3294/Vino/Index");
+                        }
+                        return Redirect("http://localhost:3294/Usuario/Index");
+                    }
+                }
+                ViewBag.ErrorMsg = "¡No se logró establecer comunicación...!";
+                return View(usr);
+            }
+
+
+
+        }
+
 	}
 }
